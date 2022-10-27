86780d8446db8b3c606a24f11ba7c21c8dea2992
hameedaah <abhameedat@gmail.com>
2022-10-27 09:41:31 +0100

    final

--- src/components/js/albumData.js
+++ src/components/js/albumData.js
@@ -1,58 +1,64 @@
+import first from '../images/Ecommerce/dresses.jpg'
+import second from '../images/Ecommerce/gown.jpg'
+import third from '../images/Ecommerce/shoes.jpg'
+import fourth from '../images/Ecommerce/pexels-rfstudio-3811981.jpg'
+import fifth from '../images/Ecommerce/bags.jpg'
+import sixth from '../images/Ecommerce/shoe-closet-banner.jpg'
+import seventh from '../images/Ecommerce/istockphoto-1239168207-612x612.jpg'
+
 const albumData = [
     {
         id: 1,
-        image: 'https://images.unsplash.com/photo-1599309329365-0a9ed45a1da3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZhc2hpb24lMjBtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        image: first
        
     },
      {
         id: 2,
-        image: 'https://images.pexels.com/photos/8425376/pexels-photo-8425376.jpeg?auto=compress&cs=tinysrgb&w=600'
+        image: fourth
     },
      {
         id: 3,
-         image: 'https://wallpapercave.com/wp/wp10161922.jpg'
+         image: third
     },
      {
         id: 4,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8kFct8zK0JxNHwp1YofEHUpGSf2hmPMl7A&usqp=CAU'
+        image: second
     },
      {
         id: 5,
-            image: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=600'
+            image: fifth
      },
        
      {
         id: 6,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTF2ZHrehkvnLX21FlhgGsMh9ZJDS5C0h_Q&usqp=CAU'
+        image: sixth
          
      }, 
      {
         id: 7,
-         image: 'https://images.pexels.com/photos/1374128/pexels-photo-1374128.jpeg?auto=compress&cs=tinysrgb&w=600'
-    
-     
+         image: seventh
     },
     
      {
         id: 8,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI-Yte08Kcy5udRSdR2t5uAzDrhFkxsSg0sQ&usqp=CAU'
+        image: 'https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600'
         
     },
      {
         id: 9,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6YtyMRrtEePzHGLDZ8knaspS3D5s-JCXCKA&usqp=CAU'
+        image: 'https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
      {
         id: 10,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYNDbJ-XrslmxvDozSzKG9L_AXThOSTlgtw&usqp=CAU'
+        image: 'https://images.pexels.com/photos/380782/pexels-photo-380782.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
      {
         id: 11,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO0-GwcR6qX4cIhDY7qHPAseyhXeHi5_jsQbZ_RwCoWaCI04tXU428O7SDgWNQ-_1ximg&usqp=CAU'
+        image: 'https://images.pexels.com/photos/1884584/pexels-photo-1884584.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
      {
         id: 12,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ-PQs4zU-LlsJdpLUHM-4JndtxCYHP0wq9A&usqp=CAU'
+        image: 'https://images.pexels.com/photos/212236/pexels-photo-212236.jpeg?auto=compress&cs=tinysrgb&w=600'
     }
 
 ]
